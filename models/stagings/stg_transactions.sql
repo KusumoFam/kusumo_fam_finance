f3a8d4492a5cde50b5465254231b0d099997bd71
Gregorius Kusumo <grego.kusumo@thinkific.com>
2022-08-28 15:49:30 -0700

    Add transations raw and staging

---
+++ models/stagings/stg_transactions.sql
@@ -0,0 +1,10 @@
+select
+    Period as date,
+    Accounts as account,
+    Category as category,
+    Subcategory as subcategory,
+    trim(Note) as vendor,
+    CAD as amount,
+    TransactionType as transaction_type,
+    Detail as detail
+from {{ source('raw_gsheets', 'transactions_raw') }}
